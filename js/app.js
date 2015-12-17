9e5d9c6e360413b4a1c521279b9661783a9082ed
kashj22 <kjavaid22@gmail.com>
2015-12-17 12:15:47 +0000

    working on jtinder. adding class to our li

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 angular 
   .module('FlicPic', ['angular-jwt', 'ngResource'])
-  .constant('API', 'http://flicpic.herokuapp.com/')
+  .constant('API', 'http://localhost:3000/api') // http://heroku.com/flicpic
   .config(function($httpProvider) {
     $httpProvider.interceptors.push('AuthInterceptor')
   });
